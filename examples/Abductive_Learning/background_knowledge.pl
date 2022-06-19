b33ccbdb188e839cecaa80b123013d1df1ecd599
Dany Varghese <gagiprha@gmail.com>
2022-06-19 22:55:00 +0100

    Add files via upload

---
+++ examples/Abductive_Learning/background_knowledge.pl
@@ -0,0 +1,60 @@
+taxacode(a).
+taxacode(b).
+taxacode(c).
+taxacode(d).
+taxacode(e).
+taxacode(f).
+
+predator(a).
+predator(b).
+predator(c).
+
+size_class(a, 4).
+size_class(b, 3).
+size_class(c, 2).
+size_class(d, 1).
+size_class(e, 1).
+size_class(f, 1).
+
+bigger(a,b). 
+bigger(a,c).
+bigger(a,d).
+bigger(a,e).
+bigger(a,f).
+bigger(b,c).
+bigger(b,d).
+bigger(b,e).
+bigger(b,f).
+bigger(c,d).
+bigger(c,e).
+bigger(c,f).
+
+
+
+
+
+
+abundance1(a, s1, down). 
+abundance1(a, s2, down).
+abundance1(a, s3, down). 
+abundance1(a, s4, down).
+abundance1(b, s1, up). 
+abundance1(b, s2, up).
+abundance1(b, s3, down). 
+abundance1(b, s4, down).
+abundance1(c, s1, down).
+abundance1(c, s2, down).
+abundance1(c, s3, up). 
+abundance1(c, s4, up). 
+abundance1(d, s1, up).
+abundance1(d, s2, up).
+abundance1(d, s3, down).
+abundance1(d, s4, down).
+abundance1(e, s1, up).  
+abundance1(e, s2, up).
+abundance1(e, s3, down).  
+abundance1(e, s4, down). 
+abundance1(f, s1, down). 
+abundance1(f, s2, down). 
+abundance1(f, s3, up). 
+abundance1(f, s4, up).
