a7097b04161408f0bcad9e193b6e406a16386c40
Dany Varghese <gagiprha@gmail.com>
2024-03-26 19:59:06 +0000

    Add files via upload

---
+++ examples/ancestor/BK.pl
@@ -0,0 +1,12 @@
+parent(queen_victoria,edward_seventh).
+parent(edward_seventh,george_fifth).
+parent(george_fifth,george_sixth).
+parent(george_sixth,elizabeth_second).
+parent(queen_mother,elizabeth_second).
+parent(elizabeth_second,prince_charles).
+parent(prince_philip,prince_charles).
+parent(prince_charles,prince_william).
+parent(prince_charles,prince_harry).
+parent(princess_diana,prince_william).
+parent(princess_diana,prince_harry).
+parent(prince_william,prince_george).
---
+++ examples/ancestor/pos_example.f
@@ -0,0 +1,3 @@
+ancestor(elizabeth_second,prince_charles).
+ancestor(george_sixth,prince_harry).
+ancestor(queen_mother,prince_william).
